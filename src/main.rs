074f4bd38617e3bd733f9bd0e721cb1dff2477c8
lyswifter <ly70835@163.com>
2022-07-21 11:17:13 +0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::net::{TcpListener, TcpStream};
+use log::{info, error};
+
+fn handle_connection() {
+   //...
+   println!("handle_connection ------");
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:4132").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                handle_connection();
+            }
+            Err(e) => {
+                println!("listener error {}", e)
+            }
+        }
+    }
+    Ok(())
+}
